bbd655fd1680b84e7baeb298efecabb6699213bf
Tolax <tolegenda@DESKTOP-SR00FHP>
2024-06-03 01:44:28 +0300

    cat

--- cat/cat_bash.h
+++ cat/cat_bash.h
@@ -1,15 +1,15 @@
-#ifndef CAT
-#define CAT
-#include <stdio.h>
+#ifndef S21_CAT
+#define S21_CAT
 #include <getopt.h>
+#include <stdio.h>
 
 struct Options {
-    int b;
-    int e;
-    int n;
-    int s;
-    int t;
-    int v;
+  int b;
+  int e;
+  int n;
+  int s;
+  int t;
+  int v;
 };
 
 int print_file(char *filename, struct Options *Options);
